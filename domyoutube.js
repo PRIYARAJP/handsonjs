1cdae2811e1852edc151dd373b9b078f9c8850cc
PRIYADHARSHINI R <122624837+PRIYARAJP@users.noreply.github.com>
2023-03-28 16:41:04 +0530

    Update domyoutube.js

--- domyoutube.js
+++ domyoutube.js
@@ -1,56 +1,50 @@
+let is=100
+let hs=null;
+let rn = generateRandomInteger(100);
 
-let initialscore=100
-let highscore=null;
-let randomnumber = generateRandomInteger(100);
-function reloadgame(){
-    document.getElementById('guessid').value="";
-    document.querySelector('.score').textContent=100
-    document.querySelector('#sc').textContent="Start guessing..."
-    document.body.style.backgroundColor="skyblue";
-    document.querySelector('.number').style.height="70px"
-    document.querySelector('.number').textContent="?";
-     randomnumber = generateRandomInteger(100);
-}
-
-function generateRandomInteger(max) {
-    return Math.floor(Math.random() * max) + 1;
-}
-  
-    
-  
-  function maincheck(){
-          
-       let inputnumber= document.querySelector('#guessid').value
-
-    if(initialscore!==0){
-       if(inputnumber>randomnumber ){
-       document.querySelector('.message').textContent="Your Guess Is High"
-           initialscore--
-         document.querySelector('.score').textContent=initialscore
-       }
-       else if(inputnumber<randomnumber){
-        document.querySelector('.message').textContent="Your Guess Is Low"
-        initialscore--
-        document.querySelector('.score').textContent=initialscore
-       }
-       else{
-
-        document.querySelector('.message').textContent="🤩🤩 Hurray You Won 🤩🤩"
-        document.body.style.backgroundColor="blue";
-        initialscore--
-        document.querySelector('.score').textContent=initialscore
-        document.querySelector('.number').textContent=randomnumber;
-        document.querySelector('.number').style.height="70px"
-         highscore=initialscore
-          document.querySelector('.highscore').textContent=highscore
-          document.querySelector('.check').style.visibility="hidden" 
-        
-             
-        
-       }
+  function maincheck(){         
+       let ip= document.querySelector('#guessid').value;
+    if(is===0){
+      document.querySelector('.message').textContent="Game Over 🙂 🙂 your score is 0"     
     } else{
-        document.querySelector('.message').textContent="Game Over 🙂 🙂 your score is 0"
-        
+      if(ip>rn ){
+        document.querySelector('.message')
+        .textContent="Your Guess Is High";
+            is--
+          document.querySelector('.score').textContent=is;
+        }
+        else if(ip<rn){
+         document.querySelector('.message')
+         .textContent="Your Guess Is Low";
+         is--
+         document.querySelector('.score').textContent=is;
+        }
+        else{
+ 
+         document.querySelector('.message').textContent="🤩🤩 Hurray You Won 🤩🤩"
+         document.body.style.backgroundColor="blue";
+         is--
+         document.querySelector('.score').textContent=is;
+         document.querySelector('.number').textContent=rn;
+         document.querySelector('.number').style.height="70px"
+          hs=is
+           document.querySelector('.highscore').textContent=hs;
+           document.querySelector('.check').style.visibility="hidden" ;
+        } 
     }
        
    }
+   function reloadgame(){
+    document.getElementById('guessid').value="";
+    document.querySelector('.score').textContent=100
+    document.querySelector('#sc')
+    .textContent="Start guessing...";
+    document.body.style.backgroundColor="skyblue";
+    document.querySelector('.number').style.height="70px"
+    document.querySelector('.number').textContent="?";
+     rn = generateRandomInteger(100);
+}
+
+function generateRandomInteger(max) {
+  return Math.floor(Math.random() * max) + 1;
+}  
